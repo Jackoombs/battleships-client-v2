f582c8cca2914d78c5544ce48786f83a8a67cec0
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-23 10:30:07 +0000

    Added header component.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="py-8">
+      <h1 className="text-center text-6xl font-semibold text-cyan-200">
+        Battleships
+      </h1>
+    </header>
+  );
+}
