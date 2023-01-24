c9e7b320179f24b2fcaf574e36ac05260e9d840a
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-24 15:15:09 +0000

    Prettier fixes.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className="py-8">
-      <h1 className="text-center text-6xl font-semibold text-cyan-200">
+      <h1 className="text-center font-semibold text-6xl text-cyan-100">
         Battleships
       </h1>
     </header>
