a5604d5137bb9dc2f15111badf157e1e0f71b7de
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-24 20:11:53 +0000

    Added two functions to style tile. Added a handle click function to remove or place ship.

--- src/components/GameBoard/PlanningTile.tsx
+++ src/components/GameBoard/PlanningTile.tsx
@@ -10,9 +10,11 @@ interface Props {
     React.SetStateAction<[number, number] | null>
   >;
   updateHighlightedTiles: (tile: [number, number]) => void;
+  areHighlightedTilesValid: boolean;
   highlightColor: string | undefined;
   highlightedTiles: [number, number][];
   placeShip: () => void;
+  removeShip: (id: ShipConstructor["id"]) => void;
 }
 
 export function PlanningTile({
@@ -20,12 +22,15 @@ export function PlanningTile({
   y,
   tileStatus,
   setLastHoveredTile,
+  areHighlightedTilesValid,
   updateHighlightedTiles,
   highlightColor,
   highlightedTiles,
   placeShip,
+  removeShip,
 }: Props) {
   const ships = useShipsContext();
+  const activeShip = ships.getActiveShip();
   const isHighlighted = (
     arr: [number, number][],
     target: [number, number]
@@ -37,27 +42,57 @@ export function PlanningTile({
   };
 
   const tileColor = (x: number, y: number) => {
+    if (
+      !areHighlightedTilesValid &&
+      isHighlighted(highlightedTiles, [x, y]) &&
+      activeShip
+    ) {
+      return "bg-gray-600";
+    }
     if (typeof tileStatus === "string") {
       return ships.getShipByID(tileStatus)?.color;
     }
-    return x % 2 === y % 2 ? "bg-teal-100" : "bg-teal-300";
+    if (isHighlighted(highlightedTiles, [x, y]) && activeShip) {
+      return activeShip.highlightColor;
+    }
+    return x % 2 === y % 2 ? "bg-cyan-200" : "bg-teal-300";
+  };
+
+  const isCursorPointer = () => {
+    if (
+      (isHighlighted(highlightedTiles, [x, y]) &&
+        activeShip &&
+        areHighlightedTilesValid) ||
+      (typeof tileStatus === "string" && !activeShip)
+    ) {
+      return "cursor-pointer";
+    }
+  };
+
+  const handleClick = () => {
+    if (!activeShip && typeof tileStatus === "string") {
+      const ship = ships.getShipByID(tileStatus);
+      removeShip(tileStatus);
+      ships.setActiveShip(ship?.name);
+    } else {
+      placeShip();
+    }
   };
 
   return (
-    <button
+    <div
       className={clsx(
         "aspect-square w-full md:w-14 lg:w-16 border-cyan-800 border sm:border-2 duration-100 rounded sm:rounded-lg",
-        isHighlighted(highlightedTiles, [x, y]) && ships.getActiveShip()
-          ? ships.getActiveShip()?.highlightColor
-          : tileColor(x, y)
+        tileColor(x, y),
+        isCursorPointer()
       )}
-      onClick={placeShip}
+      onClick={handleClick}
       onPointerEnter={() => {
         setLastHoveredTile([x, y]);
         updateHighlightedTiles([x, y]);
       }}
     >
       {`${x}, ${y}`}
-    </button>
+    </div>
   );
 }
