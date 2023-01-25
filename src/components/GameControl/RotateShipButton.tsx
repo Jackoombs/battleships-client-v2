4b42ed62b9e5a30fa5559c71728b45cb55da6fec
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-25 00:04:42 +0000

    Added a button to rotate ship on mobile.

---
+++ src/components/GameControl/RotateShipButton.tsx
@@ -0,0 +1,16 @@
+import { MdScreenRotation } from "react-icons/md";
+
+interface Props {
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const RotateShipButton = ({ setState }: Props) => {
+  return (
+    <button
+      className="w-full px-8 py-4 flex justify-center text-slate-900 rounded font-semibold text-lg uppercase tracking-widest hover:text-cyan-100 hover:bg-slate-900 duration-150 bg-cyan-100"
+      onClick={() => setState((state) => !state)}
+    >
+      <MdScreenRotation className="text text-3xl" />
+    </button>
+  );
+};
