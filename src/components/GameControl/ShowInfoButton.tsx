2a1e3cdfe9ee6ca65364d8016b6b55fbd87df2ce
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-27 20:00:17 +0000

    Fixed prop issue.

--- src/components/GameControl/ShowInfoButton.tsx
+++ src/components/GameControl/ShowInfoButton.tsx
@@ -16,7 +16,7 @@ export const ShowInfoButton = () => {
         <IoInformationOutline className="text-3xl" />
       </button>
       {modalOpen && (
-        <Modal {...{ setModalOpen }}>
+        <Modal callBack={() => setModalOpen(false)}>
           <div>
             <Instructions />
             <button
