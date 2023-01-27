72ad33dc92e020fc5e1231b16905a1f5280caab9
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-27 19:51:35 +0000

    Deleted unused header.

--- src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-export function Header() {
-  return (
-    <header className="py-8">
-      <h1 className="text-center font-semibold text-6xl text-cyan-100">
-        Battleships
-      </h1>
-    </header>
-  );
-}
