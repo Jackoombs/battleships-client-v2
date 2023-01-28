13d1048149849c512d4c66f851d3c8790f80a0f2
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-01-28 18:15:22 +0000

    Rendering logic now a function.

--- src/components/GameBoard/BoardController.tsx
+++ src/components/GameBoard/BoardController.tsx
@@ -1,13 +1,23 @@
 import { useGameContext } from "../../hooks/useGameContext";
+import { OpponentBoard } from "./OpponentBoard";
 import { PlanningBoard } from "./PlanningBoard";
+import { PlayerBoard } from "./PlayerBoard";
 
 export const BoardController = () => {
-  const game = useGameContext();
-  const gamePhase = game.gamePhase;
+  const { gamePhase, playerTurn } = useGameContext();
+
+  const renderBoard = () => {
+    if (gamePhase === "planning") {
+      return <PlanningBoard />;
+    }
+    if (gamePhase === "battle") {
+      return playerTurn ? <OpponentBoard /> : <PlayerBoard />;
+    }
+  };
 
   return (
     <div className="lg:p-10 md:p-6 2xl:p-16 w-full lg:w-max md:rounded-3xl lg:bg-slate-700">
-      {gamePhase === "planning" && <PlanningBoard />}
+      {renderBoard()}
     </div>
   );
 };
