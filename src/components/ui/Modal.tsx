9f2e58cbde38e1a2b9e8a4f9c3cf09276b9a179b
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-14 15:18:54 +0000

    Made button on modal optional.

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -2,16 +2,21 @@ import { GrFormClose } from "react-icons/gr";
 
 interface Props {
   children: string | JSX.Element | JSX.Element[];
-  callBack: (...args: any) => any;
+  callBack?: (...args: any) => any;
 }
 
 export const Modal = ({ children, callBack }: Props) => {
   return (
     <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-90 flex justify-center items-center">
       <div className="max-w-2xl relative bg-cyan-100 w-full m-3 py-10 px-4 rounded-xl text-slate-900">
-        <button onClick={callBack} className="absolute right-4 top-4 text-4xl">
-          <GrFormClose />
-        </button>
+        {callBack && (
+          <button
+            onClick={callBack}
+            className="absolute right-4 top-4 text-4xl"
+          >
+            <GrFormClose />
+          </button>
+        )}
         {children}
       </div>
     </div>
