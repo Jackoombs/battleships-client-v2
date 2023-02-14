45a7e854cc4278413e43cdad76bd19d6aca74032
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-14 15:20:49 +0000

    Added socket function to handle receiving fire. Added socket function to hadle round end and game result.

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { ShipId } from "../vite-env";
 import { GameType } from "./useGame";
+import { ShipsType } from "./useShips";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL);
 interface Props {
   game: GameType;
+  ships: ShipsType;
 }
 
-export const useSocket = ({ game }: Props) => {
+export const useSocket = ({ game, ships }: Props) => {
   const [room, setRoom] = useState<string | null>(null);
   const [playerShipsReady, setPlayerShipsReady] = useState(false);
 
@@ -39,13 +42,45 @@ export const useSocket = ({ game }: Props) => {
       }
     });
 
+    socket.on("opponentReceiveFire", (coord: [number, number]) => {
+      const [x, y] = coord;
+      let isSunk: ShipId | null = null;
+      const isHit = game.checkIsHit(coord);
+      if (isHit) {
+        const shipId = game.playerBoard[x][y] as ShipId;
+        isSunk = game.checkIsSunk(shipId);
+        ships.updateShipOnHit(shipId, coord);
+      }
+      return socket.emit("endRound", room, coord, isHit, isSunk);
+    });
+
+    socket.on(
+      "endRound",
+      (coord: [number, number], isHit: boolean, isSunk: ShipId | null) => {
+        if (game.playerTurn) {
+          game.updateOpponentBoard(coord, isHit ? "H" : "M");
+        } else {
+          game.updatePlayerBoard(coord, isHit ? "H" : "M");
+        }
+        game.updateRoundResultMessage(isHit, isSunk);
+        game.setLatestTileTarget(coord);
+      }
+    );
+
+    socket.on("gameResult", (isWinner: boolean) => {
+      game.setGamePhase("result");
+      game.setIsWinner(isWinner);
+    });
+
     return () => {
       socket.off("updateGamePhase");
       socket.off("connectToRoom");
       socket.off("disconnectFromRoom");
       socket.off("checkPlayerReady");
+      socket.off("opponentReceiveFire");
+      socket.off("endRound");
     };
-  }, [playerShipsReady]);
+  }, [playerShipsReady, ships, game.planningBoard]);
 
   const requestRoom = (room: string, createOrJoin: "create" | "join") => {
     socket.emit("requestRoom", room, createOrJoin);
@@ -60,6 +95,11 @@ export const useSocket = ({ game }: Props) => {
     socket.emit("checkOpponentReady", room);
   };
 
+  const playerFire = (coord: [number, number]) => {
+    console.log("fire!");
+    socket.emit("playerFire", room, coord);
+  };
+
   return {
     socket,
     room,
@@ -69,6 +109,7 @@ export const useSocket = ({ game }: Props) => {
     playerShipsReady,
     setPlayerShipsReady,
     checkOpponentReady,
+    playerFire,
   };
 };
 
