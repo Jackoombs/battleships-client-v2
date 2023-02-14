55b677377854a58c5562f99c0fa5334233581369
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-14 16:02:56 +0000

    Added text depending on if winner or loser. Added a button to reset the game.

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,18 +1,49 @@
 import Confetti from "react-confetti";
 import { createPortal } from "react-dom";
 import { useGameContext } from "../../hooks/useGameContext";
+import { useSocketContext } from "../../hooks/useSocketContext";
+import { Button } from "../ui/Button";
 import { Text } from "../ui/Text";
 
 export const Result = () => {
   const { isWinner } = useGameContext();
+  const { resetGame } = useSocketContext();
 
   return (
     <>
       {createPortal(
         <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-90 flex justify-center items-center">
           <div className="max-w-2xl relative bg-cyan-100 w-full m-3 py-10 px-4 rounded-xl text-slate-900">
-            <Text theme="dark">{isWinner.toString()}</Text>
+            {isWinner && (
+              <>
+                <h2 className="text-5xl text-center font-semibold">You Won!</h2>
+                <Text className="py-6" size="lg" theme="dark">
+                  Congratulations on beating your opponent! Your strategic
+                  thinking paid off, and you were able to successfully sink all
+                  of their ships. Your victory is a testament to your skill and
+                  perseverance. Well done!
+                </Text>
+              </>
+            )}
+            {!isWinner && (
+              <>
+                <h2 className="text-5xl text-center font-semibold">
+                  You Lost!
+                </h2>
+                <Text className="py-6" size="lg" theme="dark">
+                  Losing this round of Battleships may not have gone your way,
+                  but don't worry, there's always the next game! Remember, it's
+                  all about having fun and trying new strategies. Who knows,
+                  maybe next time you'll come out on top and sink all your
+                  opponent's ships!
+                </Text>
+              </>
+            )}
+            <Button callback={() => resetGame()} theme="dark">
+              Play Again?
+            </Button>
           </div>
+
           {isWinner && (
             <Confetti width={window.innerWidth} height={window.innerHeight} />
           )}
