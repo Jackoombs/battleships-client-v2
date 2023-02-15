7f69dbef16d1769808b5ad0472ad0dd5ffef5cd3
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-15 13:35:21 +0000

    Hover effect no longer applies on mobile.

--- src/components/GameControl/RotateShipButton.tsx
+++ src/components/GameControl/RotateShipButton.tsx
@@ -8,7 +8,7 @@ export const RotateShipButton = ({ setState }: Props) => {
   return (
     <button
       aria-label="rotate ship button"
-      className="w-full px-8 py-4 flex justify-center text-slate-900 rounded font-semibold text-lg uppercase tracking-widest hover:text-cyan-100 hover:bg-slate-900 duration-150 bg-cyan-100"
+      className="w-full px-8 py-4 flex justify-center text-slate-900 rounded font-semibold text-lg uppercase tracking-widest sm:hover:text-cyan-100 sm:hover:bg-slate-900 duration-150 bg-cyan-100"
       onClick={() => setState((state) => !state)}
     >
       <MdScreenRotation className="text text-3xl" />
