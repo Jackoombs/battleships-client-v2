5e6d74d358334b326abfcddbc64a8038506b1a3b
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-15 13:30:19 +0000

    Altered padding slightly.

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Modal = ({ children, callBack }: Props) => {
   return (
     <div className="z-50 fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-90 flex justify-center items-center">
-      <div className="max-w-2xl relative bg-cyan-100 w-full m-3 py-10 px-4 rounded-xl text-slate-900">
+      <div className="max-w-2xl relative bg-cyan-100 w-full m-3 pt-20 pb-12 px-4 rounded-xl text-slate-900">
         {callBack && (
           <button
             onClick={callBack}
