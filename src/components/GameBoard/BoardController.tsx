c882868752cf7531edf1b84680b1a7b048c02853
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-15 11:46:38 +0000

    Added mobile buttons for battle phase and small style change.

--- src/components/GameBoard/BoardController.tsx
+++ src/components/GameBoard/BoardController.tsx
@@ -1,4 +1,8 @@
 import { useGameContext } from "../../hooks/useGameContext";
+import { MobileControls } from "../GameControl/MobileControls";
+import { ShowInfoButton } from "../GameControl/ShowInfoButton";
+import { TurnIndicator } from "../GameControl/TurnIndicator";
+import { ResultMessage } from "../Sidebar/ResultMessage";
 import { OpponentBoard } from "./OpponentBoard";
 import { PlanningBoard } from "./PlanningBoard";
 import { PlayerBoard } from "./PlayerBoard";
@@ -11,12 +15,23 @@ export const BoardController = () => {
       return <PlanningBoard />;
     }
     if (gamePhase === "battle") {
-      return playerTurn ? <OpponentBoard /> : <PlayerBoard />;
+      return (
+        <>
+          {playerTurn ? <OpponentBoard /> : <PlayerBoard />}
+          <div className="pt-4 lg:hidden flex flex-col w-full gap-3">
+            <div className="w-full grid grid-cols-[5fr_1fr] gap-3 md:pt-14">
+              <TurnIndicator />
+              <ShowInfoButton />
+            </div>
+            <ResultMessage />
+          </div>
+        </>
+      );
     }
   };
 
   return (
-    <div className="lg:p-10 md:p-6 2xl:p-16 w-full lg:w-max md:rounded-3xl lg:bg-slate-700">
+    <div className="lg:p-10 md:p-6 2xl:p-16 w-full lg:w-max md:rounded-3xl scroll">
       {renderBoard()}
     </div>
   );
